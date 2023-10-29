b9ff68a6af4807d861e69f836b3a8e942994b61b
Fraze666 <129980843+Fraze666@users.noreply.github.com>
2023-10-29 11:22:24 +0300

    Fraze
    добавил жнеца короля бинтов

--- HalloweenGameJam/Assets/scripts/EnemyPaper/EnemyTrigger.cs
+++ HalloweenGameJam/Assets/scripts/EnemyPaper/EnemyTrigger.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+<<<<<<< Updated upstream
 
 public class EnemyTrigger : MonoBehaviour
 {
     // Start is called before the first frame update
+=======
+using UnityEngine.Events;
+
+public class EnemyTrigger : MonoBehaviour
+{
+    public static UnityEvent ActivateTriggerEnemyPaper = new UnityEvent();
+>>>>>>> Stashed changes
     void Start()
     {
         
     }
 
+<<<<<<< Updated upstream
     // Update is called once per frame
+=======
+    
+>>>>>>> Stashed changes
     void Update()
     {
         
     }
+<<<<<<< Updated upstream
+=======
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ActivateTriggerEnemyPaper.Invoke();
+            print("Zarabotalo");
+        }
+    }
+>>>>>>> Stashed changes
 }
